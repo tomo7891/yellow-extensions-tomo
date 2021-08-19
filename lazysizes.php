171d681bf7dce3fffcdb2f606a48fcff21c41a59
tomo <44656779+tomo7891@users.noreply.github.com>
2021-08-19 19:36:55 +0900

    Add files via upload

---
+++ lazysizes.php
@@ -0,0 +1,34 @@
+<?php
+//https://github.com/aFarkas/lazysizes
+class YellowLazysizes
+{
+    const VERSION = "0.8.10";
+    public $yellow;         // access to API
+
+
+    public function onLoad($yellow)
+    {
+        $this->yellow = $yellow;
+    }
+
+    public function onParsePageExtra($page, $name)
+    {
+        $output = null;
+        if ($name == 'header') {
+                $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
+                //$output .= "<script type=\"text/javascript\" src=\"{$extensionLocation}lazysizes.min.js\"></script>\n";
+                $output .= "<script type=\"text/javascript\" src=\"https://cdn.jsdelivr.net/npm/lazysizes@5.3.2/lazysizes.min.js\"></script>\n";
+        }
+        return $output;
+    }
+
+    public function onParsePageOutput($page, $text)
+    {
+        $output = null;
+        $text = preg_replace_callback('/<img([^>]*)>/', function ($matches) {
+            $match = rtrim($matches[1], '/');
+            return '<img'. $match .' loading="lazy">';
+        }, $text);
+        return $text;
+      }
+}
