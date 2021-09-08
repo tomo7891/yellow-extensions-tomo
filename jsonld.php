7d48f4b17b03cd6f69fe1cab7cd378b44762fc20
tomo <44656779+tomo7891@users.noreply.github.com>
2021-09-08 21:03:55 +0900

    Add files via upload

---
+++ jsonld.php
@@ -0,0 +1,45 @@
+<?php
+// Json-LD extension
+
+class YellowJsonld
+{
+  const VERSION = "0.8.18";
+  public $yellow;         // access to API
+
+  // Handle initialization
+  public function onLoad($yellow)
+  {
+    $this->yellow = $yellow;
+  }
+
+  public function onParsePageExtra($page, $name)
+  {
+    $output = null;
+    if ($name == 'header') {
+      $list = null;
+      $currentLocation = $this->yellow->page->getLocation(true);
+      $pages = $this->yellow->content->path($currentLocation, true);
+      $i = 1;
+      foreach ($pages as $page) {
+        $list[] = '{
+          "@type": "ListItem",
+          "position": '.$i.',
+          "name": "'.$page->getHtml("title").'",
+          "item": "'.$page->getUrl().'"
+        }';
+        $i++;
+      }
+      if($list > 0){
+        $output = '<script type="application/ld+json">{';
+          $output .= '"@context": "https://schema.org/",';
+          $output .= '"@type": "BreadcrumbList",';
+          $output .= '"@name": "パンくずリスト",';
+          $output .= '"itemListElement": [';
+          $output .= implode(',',$list);
+          $output .= ']';
+          $output .= '}</script>';
+        }
+    }
+    return $output;
+  }
+}
