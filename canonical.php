ffcbe13014e9dc8dbf3a522448dce008b87a5c33
tomo <44656779+tomo7891@users.noreply.github.com>
2021-09-13 13:42:05 +0900

    Add files via upload

--- canonical.php
+++ canonical.php
@@ -15,24 +15,10 @@ class YellowCanonical
   public function onParsePageExtra($page, $name)
   {
     $output = null;
+
     if ($name == 'header') {
-      $url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-
-      //normalization
-      if(strpos($url, 'www')){
-        $url = str_replace('www.','',$url);
-        header("HTTP/1.1 301 Moved Permanently");
-        header("Location: {$url}");
-        exit;
-      }
-
-      if (empty($_SERVER['HTTPS'])) {
-        header("HTTP/1.1 301 Moved Permanently");
-        header("Location: {$url}");
-        exit;
-      }
-
       //set canonical url
+      $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
       if ($this->yellow->page->getHtml("canonical")) {
         $url = $this->yellow->page->getHtml("canonical");
       } else {
@@ -43,5 +29,23 @@ class YellowCanonical
       $output .= "<link rel=\"canonical\" href=\"{$url}\">\n";
     }
     return $output;
+  }
+}
+
+if (PHP_SAPI != "cli") {
+  $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+  //normalization
+  if (strpos($url, 'www') && !strpos($url, 'localhost')) {
+    $url = str_replace('www.', '', $url);
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: {$url}");
+    exit;
+  }
+
+  if (empty($_SERVER['HTTPS']) && !strpos($url, 'localhost')) {
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: {$url}");
+    exit;
   }
 }
