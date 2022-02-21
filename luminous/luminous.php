2b3c3af75a990bd68a0b9fde62f02336b2eb198c
tomo <44656779+tomo7891@users.noreply.github.com>
2022-02-21 18:15:11 +0900

    Add files via upload

--- luminous/luminous.php
+++ luminous/luminous.php
@@ -17,14 +17,12 @@ class YellowLuminous {
     public function onParsePageExtra($page, $name) {
         $output = null;    
         $extensionLocation = $this->yellow->system->get("coreServerBase") . $this->yellow->system->get("coreExtensionLocation");    
-        if ($name == 'header'){          
-            $css = "{$extensionLocation}luminous.css";
-            $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$css}\" />";
+        if ($name == 'header'){
+            $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}luminous.css\" />";
         }
         if ($name == 'footer') {          
           $class = $this->yellow->system->get("luminousClass");
-          $js = "{$extensionLocation}luminous.js";
-          $output .= "<script type=\"text/javascript\" src=\"{$js}\"></script>\n";
+          $output .= "<script type=\"text/javascript\" src=\"{$extensionLocation}luminous.js\"></script>\n";
           $output .= "<script>";
           $output .= "new LuminousGallery(document.querySelectorAll('.{$class}'));";
           $output .= "</script>";
