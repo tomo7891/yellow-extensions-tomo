5447689d17239f6d039b723c6bbfe677c6bd96b7
tomo <44656779+tomo7891@users.noreply.github.com>
2022-03-18 19:56:32 +0900

    Create phpinfo.php

---
+++ phpinfo/phpinfo.php
@@ -0,0 +1,29 @@
+<?php
+
+class YellowPhpinfo
+{
+    const VERSION = "0.8.14";
+    public $yellow;         // access to API
+
+    // Handle initialisation
+    public function onLoad($yellow)
+    {
+        $this->yellow = $yellow;
+    }
+
+    // Handle page meta data
+    public function onParseMeta($page)
+    {
+        if ($page->get("layout") == "phpinfo") $page->visible = false;
+    }
+
+    // Handle page layout
+    public function onParsePageLayout($page, $name)
+    {
+        if ($this->yellow->page->get("layout") == "phpinfo") {
+            if ($this->yellow->extension->isExisting("edit") && !$this->yellow->extension->get("edit")->editable) {
+                $this->yellow->page->error(404);
+            }
+        }
+    }
+}
