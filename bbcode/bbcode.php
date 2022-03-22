17355f9cff80a9735964eedc714da15450393d8a
tomo <44656779+tomo7891@users.noreply.github.com>
2022-03-22 18:44:20 +0900

    Add files via upload

--- bbcode/bbcode.php
+++ bbcode/bbcode.php
@@ -14,11 +14,14 @@ class YellowBbcode
 
   public function onParsePageOutput($page, $text)
   {
-    $otuput = null;
-    $output = $this->showBBcodes($text);
+    $output = null;
+    if (!$this->yellow->extension->get("edit")->editable) {
+      $output = $this->showBBcodes($text);
+    }
     return $output;
   }
 
+  public
   /** 
    * A simple PHP BBCode Parser function
    *
@@ -28,35 +31,27 @@ class YellowBbcode
 
   //BBCode Parser function
 
-  public function showBBcodes($text)
+  function showBBcodes($text)
   {
 
     // NOTE : I had to update this sample code with below line to prevent obvious attacks as pointed out by many users.
     // Always ensure that user inputs are scanned and filtered properly.
-    $text  = htmlspecialchars($text, ENT_QUOTES, 'utf-8');
+    //$text  = htmlspecialchars($text, ENT_QUOTES, 'utf-8');
 
     // BBcode array
     $find = array(
-      '~\[b\](.*?)\[/b\]~s',
-      '~\[i\](.*?)\[/i\]~s',
       '~\[u\](.*?)\[/u\]~s',
-      '~\[quote\](.*?)\[/quote\]~s',
       '~\[size=(.*?)\](.*?)\[/size\]~s',
       '~\[color=(.*?)\](.*?)\[/color\]~s',
-      '~\[url\]((?:ftp|https?)://.*?)\[/url\]~s',
-      '~\[img\](https?://.*?\.(?:jpg|jpeg|gif|png|bmp))\[/img\]~s'
+      '~<p>\[cite\](.*?)\[/cite\]</p>~'
     );
 
     // HTML tags to replace BBcode
     $replace = array(
-      '<b>$1</b>',
-      '<i>$1</i>',
-      '<span style="text-decoration:underline;">$1</span>',
-      '<pre>$1</' . 'pre>',
+      '<u>$1</u>',
       '<span style="font-size:$1px;">$2</span>',
       '<span style="color:$1;">$2</span>',
-      '<a href="$1">$1</a>',
-      '<img src="$1" alt="" />'
+      '<cite>$1</cite>'
     );
 
     // Replacing the BBcodes with corresponding HTML tags
