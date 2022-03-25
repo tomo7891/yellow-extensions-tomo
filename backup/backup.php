07d23d0f7728aec7894d972fb97f85a0c3db95eb
tomo <44656779+tomo7891@users.noreply.github.com>
2022-03-25 17:05:46 +0900

    Add files via upload

--- backup/backup.php
+++ backup/backup.php
@@ -1,8 +1,5 @@
 <?php
 // Backup extension
-// TODO: default settings
-// TODO: zip archive
-// TODO: exclude files
 // TODO: backup List
 // TODO: backup Limit
 
@@ -16,6 +13,7 @@ class YellowBackup
     {
         $this->yellow = $yellow;
         $this->yellow->system->setDefault("backupDirectory", "system/backup/");
+        $this->yellow->system->setDefault("backupLimit", "5");
     }
 
     // Handle page meta data
@@ -30,7 +28,7 @@ class YellowBackup
         $output = null;
         if ($name == "header" && $page->get("layout") == "backup") {
             $extensionLocation = $this->yellow->system->get("coreServerBase") . $this->yellow->system->get("coreExtensionLocation");
-            $output .= "<style>#backupform label {cursor:pointer;}.directory {margin-top:1rem;font-weight:bold;}.subdirectory{display:inline-block;padding-right:1rem;}.submit{margin-top:1rem;}</style>";
+            $output .= "<style>#backupform label {cursor:pointer;}.directory {margin-top:1rem;font-weight:bold;}.subdirectory{display:inline-block;padding-right:1rem;}.submit{margin-top:1rem;}.submit input{margin-right:1rem;}</style>";
         }
         return $output;
     }
@@ -45,7 +43,7 @@ class YellowBackup
                     $request = explode("/", trim($request, "/"));
                     $content = $media = $system = array();
                     $backupDirectory = "./" . $this->yellow->system->get("backupDirectory");
-                    $backupDirectory = $backupDirectory . date("Y-m-d-h-i-s") . "/";
+                    $backupDirectory = $backupDirectory . date("Y-m-d-h-i-s");
                     $k = $v = null;
                     foreach ($request as $key => $value) {
                         list($k, $v) = explode("=", $value);
@@ -65,14 +63,26 @@ class YellowBackup
                         if (count($array) > 0) {
                             foreach ($array as $a) {
                                 if ($a == "./" . $folder) {
-                                    $this->copyProcessor($page, "./" . $a, $backupDirectory);
+                                    $this->copyProcessor($page, $a, $backupDirectory);
                                     break;
                                 } else {
-                                    $this->copyProcessor($page, "./" . $a, $backupDirectory);
+                                    $this->copyProcessor($page, $a, $backupDirectory);
                                     continue;
                                 }
                             }
                         }
+                    }
+                    $this->zip($backupDirectory, $backupDirectory . '.zip');
+                    $this->yellow->toolbox->deleteDirectory($backupDirectory);
+                }
+                $backupZipDirectory = "./" . $this->yellow->system->get("backupDirectory");
+                $zips = $this->yellow->toolbox->getDirectoryEntries($backupZipDirectory, "/.*/", true, false, false);
+                if ($this->yellow->system->get("backupLimit") != 0 && count($zips) > $this->yellow->system->get("backupLimit")) {
+                    $files = $this->yellow->toolbox->getDirectoryEntries($backupZipDirectory, "/.*/", true, false, false);
+                    $newer = array_splice($this->yellow->toolbox->getDirectoryEntries($backupZipDirectory, "/.*/", true, false, false), -$this->yellow->system->get("backupLimit"));
+                    $delete = array_diff($files, $newer);
+                    foreach ($delete as $del) {
+                        $this->yellow->toolbox->deleteFile($backupZipDirectory . $del);
                     }
                 }
             } else {
@@ -83,11 +93,38 @@ class YellowBackup
 
     public function copyProcessor($page, $path, $backupDirectory)
     {
-        $files = $this->yellow->toolbox->getDirectoryEntriesRecursive($path, "/.*/", false, false);
+        $files = $this->yellow->toolbox->getDirectoryEntriesRecursive("./" . $path, "/.*/", false, false);
         foreach ($files as $file) {
             if (strpos($file, $backupDirectory) !== true) {
-                $this->yellow->toolbox->copyFile($file, $backupDirectory . ltrim($file, './'), true);
+                $this->yellow->toolbox->copyFile($file, $backupDirectory  . "/" . ltrim($file, './'), true);
             }
         }
+    }
+
+    public function zip($path, $zipfile)
+    {
+        $za = new ZipArchive();
+        $za->open($zipfile, ZIPARCHIVE::CREATE);
+        $this->zipSub($za, $path);
+        $za->close();
+    }
+
+    public function zipSub($za, $path, $parentPath = '')
+    {
+        $dh = opendir($path);
+        while (($entry = readdir($dh)) !== false) {
+            if ($entry == '.' || $entry == '..') {
+            } else {
+                $localPath = $parentPath . $entry;
+                $fullpath = $path . '/' . $entry;
+                if (is_file($fullpath)) {
+                    $za->addFile($fullpath, $localPath);
+                } else if (is_dir($fullpath)) {
+                    $za->addEmptyDir($localPath);
+                    $this->zipSub($za, $fullpath, $localPath . '/');
+                }
+            }
+        }
+        closedir($dh);
     }
 }
