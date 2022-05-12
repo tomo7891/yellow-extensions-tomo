8f54332a1e9472ed7f3e30a173aa1675751cb241
tomo <44656779+tomo7891@users.noreply.github.com>
2022-05-12 16:39:48 +0900

    Update kana.php

--- kana/kana.php
+++ kana/kana.php
@@ -1,22 +1,25 @@
 <?php
 // Convert Kana extension
 
-class YellowKana {
-  const VERSION = "0.8.19";
+class YellowKana
+{
+  const VERSION = "0.8.20";
   public $yellow;         // access to API
 
   // Handle initialization
-  public function onLoad($yellow) {
+  public function onLoad($yellow)
+  {
     $this->yellow = $yellow;
     $this->yellow->system->setDefault("kanaConvert", "on");
     $this->yellow->system->setDefault("kanaOption", "KasV");
   }
 
-  public function onParsePageOutput($page, $text) {
-    if ($this->yellow->system->get("kanaConvert") != "on" || defined("DEBUG") && DEBUG>=1) {
-      return $text;
+  public function onParsePageOutput($page, $text)
+  {
+    $output = null;
+    if ($this->yellow->system->get("kanaConvert") == "on" || $this->yellow->system->get("coreDebugMode") < 1) {
+      $output = mb_convert_kana($text, $this->yellow->system->get("KanaOption"), 'utf-8');
     }
-    $text = mb_convert_kana($text, $this->yellow->system->get("kanaConvert"), 'utf-8');
-    return $text;
+    return $output;
   }
 }
