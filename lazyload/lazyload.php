f6534ada33ff270322d7162c50868996302bef87
tomo <44656779+tomo7891@users.noreply.github.com>
2022-11-07 18:05:41 +0900

    class only

--- lazyload/lazyload.php
+++ lazyload/lazyload.php
@@ -1,7 +1,7 @@
 <?php
 class YellowLazyload
 {
-  const VERSION = "0.8.20";
+  const VERSION = "0.8.21";
   public $yellow;         // access to API
 
 
@@ -14,7 +14,7 @@ class YellowLazyload
   {
     $output = null;
     $text = preg_replace_callback('/<(iframe|img)([^>]*)>/', function ($matches) {
-      if (strpos($matches[2], 'lazy') !== false) {
+      if (strpos($matches[2], 'class=' ) !== false && strpos($matches[2], 'lazy') !== false) {
         return '<' . $matches[1] . ' loading="lazy"' . $matches[2] . '>';
       } else {
         return '<' . $matches[1] . ' ' . $matches[2] . '>';
