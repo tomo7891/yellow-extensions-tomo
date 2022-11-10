0777455c9d503f33b254d2bb46ccddad771419f4
tomo <44656779+tomo7891@users.noreply.github.com>
2022-11-10 19:55:50 +0900

    Update lazyload.php

--- lazyload/lazyload.php
+++ lazyload/lazyload.php
@@ -1,7 +1,7 @@
 <?php
 class YellowLazyload
 {
-  const VERSION = "0.8.21";
+  const VERSION = "0.8.22";
   public $yellow;         // access to API
 
 
@@ -15,7 +15,7 @@ class YellowLazyload
     $output = null;
     $text = preg_replace_callback('/<(iframe|img)([^>]*)>/', function ($matches) {
       if (strpos($matches[2], 'class=' ) !== false && strpos($matches[2], 'lazy') !== false) {
-        return '<' . $matches[1] . ' loading="lazy"' . $matches[2] . '>';
+        return '<' . $matches[1] . $matches[2] . ' loading="lazy"' . '>';
       } else {
         return '<' . $matches[1] . ' ' . $matches[2] . '>';
       }
