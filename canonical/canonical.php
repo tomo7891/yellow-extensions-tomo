15bad6623529350dd505bd861ad75f0d2b067568
tomo <44656779+tomo7891@users.noreply.github.com>
2026-04-22 15:43:25 +0900

    Update canonical.php

--- canonical/canonical.php
+++ canonical/canonical.php
@@ -1,39 +1,38 @@
 <?php
 // Canonical extension
 
-class YellowCanonical
-{
-    const VERSION = "0.8.20";
-    public $yellow;         // access to API
+class YellowCanonical {
+    const VERSION = "0.8.22";
+    public $yellow;
 
-    // Handle initialization
-    public function onLoad($yellow)
-    {
+    public function onLoad($yellow) {
         $this->yellow = $yellow;
     }
 
-    public function onParsePageExtra($page, $name)
-    {
+    public function onParsePageExtra($page, $name) {
         $output = null;
-        if ($name == 'header') {
-            if ($page->isExisting("canonical")) {
-                if (preg_match("/^http/", $page->get("canonical"))) {
-                    $canonical = $page->get("canonical");
-                } else {
-                    $canonical = $this->getAbsoluteUrl() . $page->get("canonical");
-                }
-            } else {
-                $canonical = $this->getAbsoluteUrl() . $page->getLocation();
-            }
-            $output .= "<link rel=\"canonical\" href=\"{$canonical}\">\n";
-        }
-        return $output;
-    }
+        if ($name == "header") {
+            // デフォルトではシステムの正規URL（coreServerScheme等に基づく）を取得します
+            $canonical = $page->getUrl(true);
 
-    public function getAbsoluteUrl()
-    {
-		$protocol = ($_SERVER["HTTPS"]) ? "https" : "http";
-		$output = $protocol . '://' . $_SERVER['SERVER_NAME'];
+            // ページメタデータに "Canonical: /path/to/page" 等の指定がある場合
+            if ($page->isExisting("canonical") && !is_string_empty($page->get("canonical"))) {
+                $custom = $page->get("canonical");
+
+                // すでに http から始まるフルURLでない場合は、システム設定を使って正規化します
+                if (!preg_match("/^\w+:/", $custom)) {
+                    $canonical = $this->yellow->lookup->normaliseUrl(
+                        $this->yellow->system->get("coreServerScheme"),
+                        $this->yellow->system->get("coreServerAddress"),
+                        $this->yellow->system->get("coreServerBase"),
+                        $custom
+                    );
+                } else {
+                    $canonical = $custom;
+                }
+            }
+            $output = "<link rel=\"canonical\" href=\"" . htmlspecialchars($canonical) . "\" />\n";
+        }
         return $output;
     }
 }
